fa6dd17dd2765f8e9438616b79a2560dd82e77c0
yoonwoo_labtop <mryoonwoo@naver.com>
2022-07-07 03:21:03 +0900

    Socket을 활용한 채팅 거의 끝

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,4 +1,12 @@
-const socket = new WebSocket(`ws://${window.location.host}`);  
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
     console.log("Connectd to Server!!✔");
@@ -6,12 +14,33 @@ socket.addEventListener("open", () => {
 
 socket.addEventListener("message", (message) => {
     console.log("New message : ", message.data, "from the server.");
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
 })
 
 socket.addEventListener("close", () => {
     console.log("Disconnected from Server 😢");
 })
 
-setTimeout(() => {
-    socket.send("Hello from the Chrome ^^");
-}, 5000);  // 10초 뒤에 일어나도록
+// setTimeout(() => {
+//     socket.send("Hello from the Chrome ^^");
+// }, 5000);  // 10초 뒤에 일어나도록
+
+
+function handleSubmit(event) {
+    event.preventDefault(); // submit event가 일어났을 때 기본적으로 발생하는 이벤트(reload)를 막아주는듯
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+}
+
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+    input.value = "Created!";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
 import WebSocket from "ws";
 import express from "express";
+import { parse } from "path";
 
 const app = express();
 
@@ -9,7 +10,6 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public")); // public 폴더를 유저에게 공개?
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/")); // home만 사용할 거임
-console.log("hello");
 
 const handleListen = () => console.log('Listening on http://localhost:3000'); // ws://local~~ 이것도 가능   
 
@@ -17,14 +17,27 @@ const server = http.createServer(app);
 
 const wss = new WebSocket.Server({ server }); // 하나의 서버에서 http와 ws을 둘 다 작동시키기 위해
 
+const sockets = [];
 
 wss.on("connection", (socket) => {  // 연결된 socket에 대해 아래의 동일한 코드를 실행시켜줌
+    sockets.push(socket);
+    socket["nickname"] = "Anonymous";
     console.log("Connectd to Browser!!✔");
     socket.on("close", () => console.log("Disconnected from the Browser..."));
-    socket.on("message", (message) => {
-        console.log(message.toString('utf-8'));
+    socket.on("message", (msg) => {
+        console.log(msg.toString('utf-8'));
+        const message = JSON.parse(msg.toString('utf-8'));
+        switch (message.type) {
+            case "new_message":
+                sockets.forEach((aSocket) => 
+                    aSocket.send(`${socket.nickname} : ${message.payload}`)
+                );
+                break;
+            case "nickname":
+                socket["nickname"] = message.payload;
+                break;
+        }
     });
-    socket.send("hello!!!");
 });
 
 server.listen(3000, handleListen);
